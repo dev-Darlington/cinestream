f4d1a27876544a388ad4ee7ee00fe8a86448ce26
Darlington <darlingtonuchenna53@gmail.com>
2026-02-07 15:38:28 +0100

    fixes: Debugged codebase for 500 Internal Server Error

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,17 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { FavoritesProvider } from "@/contexts/favoritesContext";
-import Navbar from "@/components/Navbar";
+// import { FavoritesProvider } from "@/contexts/favoritesContext";
+// import Navbar from "@/components/Navbar";
+
+// export default function App({ Component, pageProps }: AppProps) {
+//   return (
+//     <FavoritesProvider>
+//       <Navbar />
+//       <Component {...pageProps} />
+//     </FavoritesProvider>
+//   );
+// }
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <FavoritesProvider>
-      <Navbar />
-      <Component {...pageProps} />
-    </FavoritesProvider>
-  );
+  return <Component {...pageProps} />;
 }
